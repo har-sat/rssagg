ffde7f01c95e93eaf291d0c1a9cf338da2712286
har-sat <harsat.ponnusamy@gmail.com>
2025-09-04 23:13:32 +0530

    godotenv added

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalf("No PORT found in the environment")
+	}
+
+	fmt.Printf("Port: %v\n", port)
 }
