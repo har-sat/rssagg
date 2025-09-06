3252a697069eef7f8eb3fcb37e792fb39414048b
har-sat <harsat.ponnusamy@gmail.com>
2025-09-06 22:55:43 +0530

    chi router and cors settings added

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/har-sat/rssagg
 go 1.25.0
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -19,5 +21,25 @@ func main() {
 		log.Fatalf("No PORT found in the environment")
 	}
 
-	fmt.Printf("Port: %v\n", port)
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	},))
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server Running on port: %v\n", port)
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Server Error: %v\n", err)
+	}
+
 }
